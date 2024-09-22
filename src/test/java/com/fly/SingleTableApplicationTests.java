29a327d1038ff7297302ada704dc43cd8b1965f5
Feiyang Shen <2491435063@qq.com>
2024-09-22 22:49:58 +0800

    更新测试文件

--- src/test/java/com/fly/SingleTableApplicationTests.java
+++ src/test/java/com/fly/SingleTableApplicationTests.java
@@ -1,27 +1,8 @@
 package com.fly;
 
-import com.fly.entity.Student;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.LocalDateTime;
 
 @SpringBootTest
 class SingleTableApplicationTests {
-    @Autowired
-    private StudentMapper studentMapper;
-
-    @Test
-    public void test() {
-        Student student = new Student();
-        student.setId(7);
-        student.setName("徐文杰");
-        student.setAge(101);
-        student.setGender((short) 1);
-        student.setUpdateTime(LocalDateTime.now());
-        studentMapper.update(student);
-        System.out.println(studentMapper.selectAll());
-    }
 
 }
